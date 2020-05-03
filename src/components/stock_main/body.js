267dafdff30a1e1d4284eb5a9d3e41bff39e5bf3
midal2 <midal2@midal2ui-MacBook-Pro.local>
2020-05-03 12:04:34 +0900

    중간작업

--- src/components/stock_main/body.js
+++ src/components/stock_main/body.js
@@ -2,23 +2,33 @@ import React, {Component, useReducer} from 'react';
 
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Chip from '@material-ui/core/Chip';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import * as NumberUtil from '../common/number_util';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   paper: {
-    height: 70,
+    height: 100,
     width: 100,
   },
   paper2: {
-    height: 70,
+    height: 100,
     width: 300,
   },
   control: {
     padding: theme.spacing(2),
+  },
+  chip: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(0.5),
+    },
   },
 }));
 
@@ -26,7 +36,7 @@ export default function Body({stockInfos}){
   const classes = useStyles();
 
   return (
-      <Grid container className={classes.root} spacing={2} direction="column">
+      <Grid container className={classes.root} spacing={1} direction="column">
         {stockInfos.map((stockInfo)=>(
             <Grid item container spacing={0} direction="column">
               <Grid item container spacing={0} >
@@ -34,18 +44,16 @@ export default function Body({stockInfos}){
                   차트 놓일 장소
                 </Paper>
                 <Paper className={classes.paper2} xs={11}>
-                  <Typography gutterBottom variant="subtitle1">
-                    {stockInfo.stockName}<div style={{display:"inline"}}>{stockInfo.stockTime}</div>
+                  <Typography gutterBottom variant="h5" component="h2">
+                  {stockInfo.stockName}({stockInfo.stockCd}) 
                   </Typography>
-                  <Typography gutterBottom variant="subtitle1">
-                    {stockInfo.stockNowValue} {stockInfo.stockIncDecSign} {stockInfo.stockIncDecValue} ({stockInfo.stockIncDecRate}%)
+                  <Typography gutterBottom variant="body2" color="textSecondary" > 
+                    {stockInfo.stockNowValue} {stockInfo.stockIncDecSign} {stockInfo.stockIncDecValue} ({stockInfo.stockIncDecRate}%)  <div style={{display:"inline"}}>{stockInfo.stockTime}</div>
                   </Typography>
+                  {stockInfo.stockStatusList.map((stockStatus)=>(
+                    <Chip variant="outlined" size="small" label={stockStatus.summary} color={NumberUtil.getRandomNumber(2) == 2? 'primary' : 'sencondary'} />
+                  ))}
                 </Paper>
-              </Grid>
-              <Grid item spacing={0} >
-                <Typography gutterBottom variant="subtitle1">
-                  Standard license
-                </Typography>
               </Grid>
             </Grid>
         ))}
--- src/components/stock_main/index.js
+++ src/components/stock_main/index.js
@@ -1,4 +1,6 @@
 import React, {Component, useEffect, useReducer} from 'react';
+import * as XmlConverter from 'xml-js';
+import axios from 'axios';
 import Title from './title';
 import Body from './body';
 import Footer from './footer';
@@ -15,6 +17,13 @@ import * as NumberUtil from '../common/number_util';
   stockIncDecValue : number '증감금액'
   stockIncDecSign : string '부호( + or - )'
   stockIncDecRate : number '증감비율(%)'
+  stockStatusList : array '주식상황목록' 
+                [ 
+                    stockStatus
+                    ----------------
+                    summary : string '상태요약메세지'
+                    value : string '세부상태명'
+                ]
 */
 
 //주식정보
@@ -22,11 +31,12 @@ const initStockInfos = [
   {
   stockTime : DateUtil.getNowDate(),
   stockName : "더미데이터" ,
-  stockCd   : "" ,
+  stockCd   : "777777" ,
   stockNowValue : "10,000",
   stockIncDecValue : "800",
   stockIncDecSign : "▼",
   stockIncDecRate : "17",
+  stockStatusList : [],
   },
 ]
 
@@ -57,20 +67,67 @@ function createController(mode = 'real'){
         obj.getData = (cbFn) => {
           let action = {};
           action.type = 'reset';
-          action.payload = [];
-
+          
+          // console.log('axios');
+          // axios('/servlet/krx.asp.XMLSise?code=035420')
+          //   .then(function (response) {
+          //     console.log(response.data)
+          //   });
+            
+            
           // 임시데이터 생성
           let itemCnt = NumberUtil.getRandomNumber(10);
+          action.payload = [];
           for (var i=0; i<itemCnt; i++){
-            action.payload.push({
+            let obj = {};
+
+            obj = {
               stockTime : DateUtil.getNowDate() ,
               stockName : "임시생성" + i ,
-              stockCd   : "" ,
+              stockCd   : "99999" + NumberUtil.getRandomNumber(9),
               stockNowValue : NumberUtil.getRandomNumber(20000),
-              stockIncDecValue : NumberUtil.getRandomNumber(1000),
+              stockIncDecValue : NumberUtil.getRandomNumber(1000, -1000),
               stockIncDecSign : "▼",
               stockIncDecRate : NumberUtil.getRandomNumber(30),
-            });  
+              stockStatusList : [],
+            };
+
+            // 데이터상태생성
+            let statusCnt = NumberUtil.getRandomNumber(5);
+            for (var x=0; x<statusCnt; x++){
+              let createdStatus = NumberUtil.getRandomNumber(3);   
+              let createStatus = (summary, value) => {return {summary, value}};
+
+              switch(createdStatus){
+                case 1:
+                  obj.stockStatusList.push(createStatus('10%증가', '10%'));
+                  break;
+
+                case 2:
+                  obj.stockStatusList.push(createStatus('20%증가', '20%'));
+                  break;
+                
+                case 3:
+                  obj.stockStatusList.push(createStatus('30%증가', '30%'));
+                  break;
+                
+                case 4:
+                  obj.stockStatusList.push(createStatus('40%증가', '40%'));
+                  break;
+                
+                case 5:
+                  obj.stockStatusList.push(createStatus('50%증가', '50%'));
+                  break;
+
+                default:
+                  console.log('createdStatus default[' + createdStatus + ']' );
+                  obj.stockStatusList.push(createStatus('0%증가', '0%'));
+                  break;
+              }
+            }
+            
+            console.dir(obj);
+            action.payload.push(obj);
           }
 
           
@@ -96,7 +153,7 @@ export default function index(){
     const id = setInterval(() => {
       console.log('startLoopStockInfo 시작');
       stockController.getData(dispatchStockInfo); 
-    }, 1000);
+    }, 5000);
     console.log('useEffect 시작');
     
     return () => {
