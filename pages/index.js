65bd65017d68dbe33de1d0d372db5ed878ec4901
changheekim <benggri@gmail.com>
2020-05-13 11:28:06 +0900

    revert

--- pages/index.js
+++ pages/index.js
@@ -4,27 +4,6 @@ import Link from 'next/link';
 
 /* CSS Modules */
 
-/* material Modules */
-import { makeStyles } from '@material-ui/core/styles';
-
-/* CUSTOM Components */
-import Layout from './site_main/layout';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexDirection : 'column',
-        minHeight: '98vh',
-    },
-    body: {
-        flex: 1,
-    },
-    footer: {
-        height:'100',
-        marginTop: 'auto',
-        background: '#333'
-    },
-}));
 
 /**
  * index.js
@@ -32,16 +11,12 @@ const useStyles = makeStyles((theme) => ({
  * 프로젝트 진입 페이지
  */ 
 const index = () => {
-    const classes = useStyles();
-
     return (
         <div>
-            <Layout>
-                <h1 className='example'>Hello!!! DMZR!</h1>
-                <Link href="/stock_main">
-                    <a>STOCK_MAIN</a>
-                </Link>
-            </Layout>
+            <h1 className='example'>Hello!!! DMZR!</h1>
+            <Link href="/stock_main">
+                <a>STOCK_MAIN</a>
+            </Link>
         </div>
     );
 };
